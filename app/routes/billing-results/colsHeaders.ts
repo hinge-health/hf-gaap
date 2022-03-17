1a48474274a37eba9d2b9eddcaf83c6da59609ad
cali3192 <ctabilas23@gmail.com>
2022-03-17 17:32:43 -0400

    hackday - populating table with some fake data - test error state

--- app/routes/billing-results/colsHeaders.ts
+++ app/routes/billing-results/colsHeaders.ts
@@ -4,11 +4,12 @@ import generateLink, { isSuccessful } from './lib';
 interface Row {
   id: number;
   taskId: number;
-  dagId: number;
+  dagId: string;
   jobName: string;
   status: string;
   totals: number;
   executionDate: string;
+  billingCycle: string;
   success: boolean;
   logs: any; // url
 }
@@ -26,14 +27,14 @@ const columns: Array<GridColDef> = [
     field: 'dagId',
     headerName: 'Dag Id',
     type: 'number',
-    width: 90,
+    width: 200,
     editable: true,
     description: `Airflow's dagId`
   },
   {
     field: 'jobName',
     headerName: 'Job Name',
-    width: 350,
+    width: 200,
     editable: true
   },
 
@@ -63,6 +64,11 @@ const columns: Array<GridColDef> = [
     width: 250
   },
   {
+    field: 'billingCycle',
+    headerName: 'Billing Cycle',
+    width: 150
+  },
+  {
     field: 'success',
     headerName: 'Success',
     type: 'boolean',
@@ -82,21 +88,95 @@ const rows: Array<Row> = [
   {
     id: 1,
     taskId: 10,
-    dagId: 1,
-    jobName: 'Tom Nook',
+    dagId: 'fake_paddington_bear_invoices',
+    jobName: 'descriptive_job_name',
     status: 'in progress',
     totals: 1200000,
     executionDate: '3/16/2022, 2:43:22 AM',
+    billingCycle: '2022-03-16',
     success: isSuccessful('total'),
     logs: 'islandhomeparadise@scam.com'
   },
   {
     id: 2,
     taskId: 13,
-    dagId: 1,
-    jobName: 'Aurora',
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
     status: 'complete',
     totals: 1200000,
+    billingCycle: '2022-03-16',
+    executionDate: '3/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 3,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'in progress',
+    totals: 1500000,
+    billingCycle: '2022-03-16',
+    executionDate: '3/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 4,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'complete',
+    totals: 12000,
+    billingCycle: '2022-03-16',
+    executionDate: '2/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 5,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'complete',
+    totals: 20000,
+    billingCycle: '2022-03-16',
+    executionDate: '3/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 6,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'complete',
+    totals: 19000,
+    billingCycle: '2022-03-16',
+    executionDate: '3/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 7,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'complete',
+    totals: 1200000,
+    billingCycle: '2022-03-16',
+    executionDate: '2/16/2022, 2:43:22 AM',
+    success: isSuccessful('total'),
+    logs: 'placeholder@here.com'
+  },
+  {
+    id: 8,
+    taskId: 13,
+    dagId: 'fake_collection_cvs',
+    jobName: 'descriptive_job_name',
+    status: 'complete',
+    totals: 1200000,
+    billingCycle: '2022-03-16',
     executionDate: '3/16/2022, 2:43:22 AM',
     success: isSuccessful('total'),
     logs: 'placeholder@here.com'
